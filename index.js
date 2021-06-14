c225c6fa1a7fdd39ac7d927911a6ce163d6cb628
Naman Gor <64084226+namangor04@users.noreply.github.com>
2021-06-14 15:05:07 +0530

    Added Download Button

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ var navLinks = document.getElementById("navLinks");
 
 function hideMenu() {
     navLinks.style.right = "-200px";
+    window.setTimeout(function() {
+        navLinks.style.display = "none";
+    }, 600);
 }
 
 function showMenu() {
-    navLinks.style.right = "0";
+    navLinks.style.display = "block";
+    window.setTimeout(function() {
+        navLinks.style.right = "0";
+    }, 5);
 }
